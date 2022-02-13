6b9c336fdc00d05915209e183ca7887ee31f0520
Nate Rivard <nate.rivard@booking.com>
2022-02-13 14:03:35 +0100

    Start of an SD Card lib

--- LmaOS.65c02/src/main.asm
+++ LmaOS.65c02/src/main.asm
@@ -56,13 +56,6 @@ RamTest:
 
     ;;; system clock is setup, turn on interrupts so they start firing
     CLI
-
-; @DisplayBootStatus:
-;     LDA #(LCD_LINE2_START)
-;     JSR LCDMoveCursor
-;     LDA #<LmaOSBootDone
-;     LDX #>LmaOSBootDone
-;     JSR LCDPrintString
     
 StartMonitor:
     ;;; on startup, we jump into the monitor
